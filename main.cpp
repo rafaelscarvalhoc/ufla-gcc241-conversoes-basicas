5817b71ad08abf3a654a6c1c5bcc39825800cca4
rafaelscarvalhoc <rafaelsouzacarvalhooo@gmail.com>
2026-05-23 03:37:33 -0300

    novo projeto conversor completo

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int bem=0;
+    //teste se o código compila e roda :)
+    cout << "Como voce ta meu nobre?" << endl;
+    cout << "1 bem ;) 2 mau :(" << endl;
+    cin>> bem;
+    if(bem==1){
+        cout << "Que bom, fico feliz por voce!" << endl;
+    }else if(bem==2){
+        cout << "Poxa, espero que as coisas melhorem praaaaaaaaaaaaaaaaaaaa voce!" << endl;
+    }
+    return 0;
+}
